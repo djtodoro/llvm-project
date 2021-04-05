cfa31eee155e00f56c61a5115674f81232367788
Stefan Pejic <stefan.pejic@syrmia.com>
2021-04-05 15:15:00 +0200

    Do not generate .set nomips16 directive.

--- llvm/lib/Target/Mips/MipsAsmPrinter.cpp
+++ llvm/lib/Target/Mips/MipsAsmPrinter.cpp
@@ -418,7 +418,7 @@ void MipsAsmPrinter::emitFunctionEntryLabel() {
 
   if (Subtarget->inMips16Mode())
     TS.emitDirectiveSetMips16();
-  else
+  else if (!Subtarget->hasNanoMips())
     TS.emitDirectiveSetNoMips16();
 
   TS.emitDirectiveEnt(*CurrentFnSym);
