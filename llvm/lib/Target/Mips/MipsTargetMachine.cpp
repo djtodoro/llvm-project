dc5f387b96d0c874adc2edd3d759544ff8378b13
Stefan Pejic <stefan.pejic@syrmia.com>
2021-11-10 14:10:14 +0100

    Better allocation for ADDU
    cherry-picks: 00fb752a03d4

--- llvm/lib/Target/Mips/MipsTargetMachine.cpp
+++ llvm/lib/Target/Mips/MipsTargetMachine.cpp
@@ -269,6 +269,7 @@ public:
   bool addRegBankSelect() override;
   bool addGlobalInstructionSelect() override;
   void addPreSched2() override;
+  bool addPreRewrite() override;
 
   std::unique_ptr<CSEConfigBase> getCSEConfig() const override;
 };
@@ -361,6 +362,11 @@ void MipsPassConfig::addPreEmitPass() {
     addPass(createMipsBranchExpansion());
 
   addPass(createMipsConstantIslandPass());
+}
+
+bool MipsPassConfig::addPreRewrite() {
+  addPass(createNanoMipsRegisterReAllocationPass());
+  return true;
 }
 
 bool MipsPassConfig::addIRTranslator() {
