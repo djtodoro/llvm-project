886d1e235c76ab6de440f116d3b7b6fcc14665a8
Faraz Shahbazker <faraz.shahbazker@mediatek.com>
2023-03-21 00:09:48 +0800

    Parse and encode control transfer instructions
    Change-Id: I073a947236a401e0f1f43d634b4c069fa9a66547

--- llvm/lib/Target/Mips/Disassembler/MipsDisassembler.cpp
+++ llvm/lib/Target/Mips/Disassembler/MipsDisassembler.cpp
@@ -275,6 +275,14 @@ static DecodeStatus DecodeBranchTarget26MM(MCInst &Inst, unsigned Offset,
                                            uint64_t Address,
                                            const MCDisassembler *Decoder);
 
+// DecodeBranchTargetMM - Decode nanoMIPS branch offset, which is
+// shifted left by 1 bit.
+template <unsigned bits>
+static DecodeStatus DecodeBranchTargetNM(MCInst &Inst,
+					   unsigned Offset,
+					   uint64_t Address,
+					   const MCDisassembler *Decoder);
+
 // DecodeJumpTargetMM - Decode microMIPS jump target, which is
 // shifted left by 1 bit.
 static DecodeStatus DecodeJumpTargetMM(MCInst &Inst, unsigned Insn,
@@ -2552,7 +2560,19 @@ static DecodeStatus DecodeBranchTarget26MM(MCInst &Inst, unsigned Offset,
   return MCDisassembler::Success;
 }
 
-static DecodeStatus DecodeJumpTargetMM(MCInst &Inst, unsigned Insn,
+template <unsigned Bits = 16>
+static DecodeStatus DecodeBranchTargetNM(MCInst &Inst,
+  unsigned Offset,
+  uint64_t Address,
+  const MCDisassembler *Decoder) {
+  int32_t BranchOffset = SignExtend32<Bits+1>(Offset);
+
+  Inst.addOperand(MCOperand::createImm(BranchOffset));
+  return MCDisassembler::Success;
+}
+
+static DecodeStatus DecodeJumpTargetMM(MCInst &Inst,
+                                       unsigned Insn,
                                        uint64_t Address,
                                        const MCDisassembler *Decoder) {
   unsigned JumpOffset = fieldFromInstruction(Insn, 0, 26) << 1;
