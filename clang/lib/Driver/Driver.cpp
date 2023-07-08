8b811463a0bfefa7219da6a7a47e85f39466bffc
Faraz Shahbazker <faraz.shahbazker@mediatek.com>
2023-07-08 17:58:49 +0800

    Don't combine assembly with other actions for nanoMIPS
    In the absence of an instruction compression pass or mechanism,
    compression for NanoMips is mediated by the assembly matcher
    finding the shortest instructions for a base mnemonic. This
    requires the compilation actions to go through a distinct assembly
    step, similar to the -via-file-asm option.
    
    Change-Id: I6eec5bed1bff1965d58578ed8624176df2c71d4f

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -4991,7 +4991,8 @@ class ToolSelector final {
         return nullptr;
     }
 
-    if (!T->hasIntegratedAssembler())
+    if (!T->hasIntegratedAssembler() ||
+	(TC.getTriple().isNanoMips() && TC.useIntegratedAs()))
       return nullptr;
 
     Inputs = CJ->getInputs();
@@ -5014,7 +5015,8 @@ class ToolSelector final {
     if (!T)
       return nullptr;
 
-    if (!T->hasIntegratedAssembler())
+    if (!T->hasIntegratedAssembler() ||
+	(TC.getTriple().isNanoMips() && TC.useIntegratedAs()))
       return nullptr;
 
     Inputs = BJ->getInputs();
