96d76928bdf461e6e96d73b4cc9bee5be0d79cee
Faraz Shahbazker <faraz.shahbazker@mediatek.com>
2023-07-21 18:39:02 +0800

    Allow square brackets in identifiers for nanoMIPS assembler
    nanoMIPS mnemonics can have an optional architectural suffix to
    disambiguate multiple instructions which have the same mnemonic.
    As per the ISA, this suffix is enclosed in square brackets, for
    example: li[48]. The assembly parser must tokenize the suffix
    along with the mnemonic as one token.
    
    Change-Id: I240d7de279800650c6b36600a8bbfd83559ae4d5

--- llvm/include/llvm/MC/MCParser/MCAsmLexer.h
+++ llvm/include/llvm/MC/MCParser/MCAsmLexer.h
@@ -47,6 +47,7 @@ protected: // Can only create subclasses.
   bool SkipSpace = true;
   bool AllowAtInIdentifier;
   bool AllowHashInIdentifier = false;
+  bool AllowBracInIdentifier =  false;
   bool IsAtStartOfStatement = true;
   bool LexMasmHexFloats = false;
   bool LexMasmIntegers = false;
@@ -150,6 +151,8 @@ public:
   void setAllowAtInIdentifier(bool v) { AllowAtInIdentifier = v; }
 
   void setAllowHashInIdentifier(bool V) { AllowHashInIdentifier = V; }
+
+  void setAllowBracInIdentifier(bool V) { AllowBracInIdentifier = V; }
 
   void setCommentConsumer(AsmCommentConsumer *CommentConsumer) {
     this->CommentConsumer = CommentConsumer;
