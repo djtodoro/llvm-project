705749fb8fef31e17b5ca156e1e35d83c14734db
Faraz Shahbazker <faraz.shahbazker@mediatek.com>
2023-11-03 12:43:44 +0800

    Add disassembler tests
    Change-Id: Ibc23c8683e5fe6067a84290bfe986351b0f94724

--- llvm/test/MC/Mips/crc/valid-nanomips.s
+++ llvm/test/MC/Mips/crc/valid-nanomips.s
@@ -1,16 +1,24 @@
 # RUN: llvm-mc %s -triple=nanomips -show-encoding -show-inst \
 # RUN:   -mattr=+crc | FileCheck %s
+# RUN: llvm-mc -filetype=obj -triple nanomips-elf -mattr=+crc %s -o - \
+# RUN:   | llvm-objdump --triple nanomips-elf --mattr=+crc -dr - | FileCheck --check-prefixes DISAS %s
 
   .set noat
   crc32b $at, $t0	# CHECK: crc32b $at, $t0   # encoding: [0x2c,0x20,0xe8,0x03]
 			# CHECK-NEXT: # <MCInst #{{.*}} CRC32B_NM
+                        # DISAS: {{.*}}: 2c 20 e8 03     crc32b  $at, $t0
   crc32h $a0, $a1	# CHECK: crc32h $a0, $a1   # encoding: [0x85,0x20,0xe8,0x07]
 			# CHECK-NEXT: # <MCInst #{{.*}} CRC32H_NM
+                        # DISAS: {{.*}}: 85 20 e8 07     crc32h  $a0, $a1
   crc32w $a3, $a4	# CHECK: crc32w $a3, $a4   # encoding: [0xe8,0x20,0xe8,0x0b]
 			# CHECK-NEXT: # <MCInst #{{.*}} CRC32W_NM
+                        # DISAS: {{.*}}: e8 20 e8 0b     crc32w  $a3, $a4
   crc32cb $at, $s0	# CHECK: crc32cb $at, $s0  # encoding: [0x30,0x20,0xe8,0x13]
 			# CHECK-NEXT: # <MCInst #{{.*}} CRC32CB_NM
+                        # DISAS: {{.*}}: 30 20 e8 13     crc32cb  $at, $s0
   crc32ch $a0, $a1      # CHECK: crc32ch $a0, $a1  # encoding: [0x85,0x20,0xe8,0x17]
 			# CHECK-NEXT: # <MCInst #{{.*}} CRC32CH_NM
+                        # DISAS: {{.*}}: 85 20 e8 17     crc32ch  $a0, $a1
   crc32cw $a3, $a4      # CHECK: crc32cw $a3, $a4  # encoding: [0xe8,0x20,0xe8,0x1b]
 			# CHECK-NEXT: # <MCInst #{{.*}} CRC32CW_NM
+                        # DISAS: {{.*}}: e8 20 e8 1b     crc32cw  $a3, $a4
--- llvm/test/MC/Mips/ginv/valid-nanomips.s
+++ llvm/test/MC/Mips/ginv/valid-nanomips.s
@@ -1,7 +1,11 @@
 # RUN: llvm-mc %s -triple=nanomips-elf -show-encoding -show-inst \
 # RUN: -mattr=+ginv | FileCheck %s
+# RUN: llvm-mc -filetype=obj -triple nanomips-elf -mattr=+ginv %s -o - \
+# RUN:   | llvm-objdump --triple nanomips-elf --mattr=+ginv -dr - | FileCheck --check-prefixes DISAS %s
 
   ginvi $a0	# CHECK: ginvi $a0     # encoding: [0x04,0x20,0x7f,0x1f]
 		# CHECK-NEXT: # <MCInst #{{.*}} GINVI_NM
+                # DISAS: {{.*}}: 04 20 7f 1f    ginvi   $a0
   ginvt $a0, 2	# CHECK: ginvt $a0, 2  # encoding: [0x44,0x20,0x7f,0x0f]
 		# CHECK-NEXT: # <MCInst #{{.*}} GINVT_NM
+                # DISAS: {{.*}}: 44 20 7f 0f    ginvt   $a0, 2
