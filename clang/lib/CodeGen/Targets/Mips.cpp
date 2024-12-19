57ea69990e470c2ecbc84921a4f84dd8d1a3cefb
Djordje Todorovic <djordje.todorovic@htecgroup.com>
2024-12-19 22:01:10 +0100

    Make sure complex types are handled correctly
        Complex float should be returned in registers, while complex double
        should be returned by reference.

--- clang/lib/CodeGen/Targets/Mips.cpp
+++ clang/lib/CodeGen/Targets/Mips.cpp
@@ -295,7 +295,17 @@ ABIArgInfo MipsABIInfo::classifyReturnType(QualType RetTy) const {
     return ABIArgInfo::getIgnore();
 
   if (isAggregateTypeForABI(RetTy) || RetTy->isVectorType()) {
-    if (Size <= 128) {
+    bool IsNanoMips = getTarget().getTriple().isNanoMips();
+    if (IsNanoMips && Size <= 64) {
+      if (RetTy->isAnyComplexType())
+        return ABIArgInfo::getDirect();
+
+      auto RetInfo = ABIArgInfo::getDirect(returnAggregateInRegs(RetTy, Size));
+      RetInfo.setInReg(true);
+      return RetInfo;
+    }
+
+    if (!IsNanoMips && Size <= 128) {
       if (RetTy->isAnyComplexType())
         return ABIArgInfo::getDirect();
 
@@ -308,12 +318,6 @@ ABIArgInfo MipsABIInfo::classifyReturnType(QualType RetTy) const {
         ArgInfo.setInReg(true);
         return ArgInfo;
       }
-    }
-
-    if (getTarget().getTriple().isNanoMips() && Size <= 64) {
-      auto RetInfo = ABIArgInfo::getDirect(returnAggregateInRegs(RetTy, Size));
-      RetInfo.setInReg(true);
-      return RetInfo;
     }
 
     return getNaturalAlignIndirect(RetTy);
